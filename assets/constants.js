ab0f6393cb47a0358794e2fcad08feca0a22c17f
Tai Nguyen <tai.nguyen@apps-cyclone.com>
2022-05-05 23:57:22 +0700

    feat(landing-page): header, section hero, section spend & check

---
+++ assets/constants.js
@@ -0,0 +1,33 @@
+export const MENUS = [
+  {
+    href: '/features',
+    name: 'Features'
+  },
+  {
+    href: '/blog',
+    name: 'Blog'
+  },
+  {
+    href: '/support',
+    name: 'Support'
+  }
+]
+
+export const BROWSERS = [
+  {
+    label: 'Chrome',
+    url: '/svgs/chrome.svg',
+  },
+  {
+    label: 'Brave',
+    url: '/svgs/brave.svg',
+  },
+  {
+    label: 'Firefox',
+    url: '/svgs/firefox.svg',
+  },{
+    label: 'Microsoft Edge',
+    url: '/svgs/microsoft-edge.svg',
+  }
+
+]
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { MENUS } from "assets/constants";
+import AtButton from "components/AtButton";
 
 const Header: React.FC = () => {
-  return <header></header>;
+	const [scroll, setScroll] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	const handleScroll = () => {
+		setScroll(window.pageYOffset > 20);
+	};
+
+	return (
+		<header
+			className={`fixed top-0 left-0 right-0 w-full z-[999] transition-all ease-in-out duration-300 ${
+				scroll ? "is-scroll" : "py-14"
+			}`}
+		>
+			<div className="max-w-[1040px] mx-auto flex items-center">
+				<Link href="/">
+					<a className="block">
+						<img src="/svgs/logo.svg" alt="logo" />
+					</a>
+				</Link>
+
+				<div className="flex items-center justify-center flex-1 gap-x-10">
+					{MENUS.map((menu, idx) => (
+						<Link href={menu.href} key={idx}>
+							<a className="py-2 block text-normal-400 font-medium font-caption transition-all ease-in duration-150 hover:text-primary-200">
+								{menu.name}
+							</a>
+						</Link>
+					))}
+				</div>
+
+				<AtButton className="shadow-type-1 px-5 py-[14px] bg-white text-black font-medium rounded-[34px] hover:bg-inherit">
+					Download
+				</AtButton>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
