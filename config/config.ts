86fe8fcc27f620375ea2c6f2d1004b9c33892c6d
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2022-06-14 06:40:51 +0700

    change config

--- config/config.ts
+++ config/config.ts
@@ -2,10 +2,9 @@ export const homeURL = "https://fewcha.app";
 export const githubURL = "https://github.com/fewcha-wallet";
 export const twitterURL = "https://twitter.com/fewchawallet";
 export const telegramURL = "https://t.me/FewchaWallet";
-export const discordURL = "https://discord.io/fewcha";
+export const discordURL = "https://fwch.io/discord";
 export const mediumURL = "https://medium.com/fewcha-wallet";
-export const chromeStoreExtURL =
-  "https://chrome.google.com/webstore/detail/ebfidpplhabeedpnhjnobghokpiioolj";
+export const chromeStoreExtURL = "https://fwch.io/chrome";
 export const firefoxStoreExtURL = "https://fewcha.app";
 export const tasks0_1URL =
   "https://fewcha.notion.site/03f9e71b1b8a47f3a2710569fcbdff3e?v=eaa14054915f4cca8d39b25e6d295379";
